c16c7a0fc675a81f4eb65836c412a674cf15ab0a
yashust-gitcopilot <291668@ust.com>
2026-06-01 16:30:21 +0530

    Create build_vector_db.py

---
+++ rag/build_vector_db.py
@@ -0,0 +1,44 @@
+import os
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+
+from rag.embeddings import embedding_model
+from config.settings import PDF_FOLDER, FAISS_DB_PATH
+
+
+def build_vector_db():
+
+    documents = []
+
+    for file in os.listdir(PDF_FOLDER):
+
+        if file.endswith(".pdf"):
+
+            loader = PyPDFLoader(
+                os.path.join(PDF_FOLDER, file)
+            )
+
+            docs = loader.load()
+
+            for d in docs:
+                d.metadata["source_file"] = file
+
+            documents.extend(docs)
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = splitter.split_documents(documents)
+
+    vectordb = FAISS.from_documents(
+        chunks,
+        embedding_model
+    )
+
+    vectordb.save_local(FAISS_DB_PATH)
+
+    return vectordb
